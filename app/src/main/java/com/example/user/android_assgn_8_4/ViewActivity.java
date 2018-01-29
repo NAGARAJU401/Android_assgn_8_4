705c74851573fc8385df2277a77060996615548a
NAGARAJU401 <aadepu.nagaraj@gmail.com>
2018-01-29 11:44:49 +0530

    Initial commit

---
+++ app/src/main/java/com/example/user/android_assgn_8_4/ViewActivity.java
@@ -0,0 +1,13 @@
+package com.example.user.android_assgn_8_4;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ViewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.view);
+    }
+}
